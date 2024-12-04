13bb366160f61f815f9dc5c146e89f4064d3912b
josealejandroprados <josealejandroprados@gmail.com>
2024-12-04 11:01:14 -0300

    creacion de los modulos necesarios del proyecto

---
+++ src/app/modules/administracion/admin-principal/admin-principal.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AdminPrincipalRoutingModule } from './admin-principal-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    AdminPrincipalRoutingModule
+  ]
+})
+export class AdminPrincipalModule { }
---
+++ src/app/modules/administracion/donations/donations.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { DonationsRoutingModule } from './donations-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    DonationsRoutingModule
+  ]
+})
+export class DonationsModule { }
