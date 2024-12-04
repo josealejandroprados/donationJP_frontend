d32d580f6c15531ac8ab890ade8921de010c93cf
josealejandroprados <josealejandroprados@gmail.com>
2024-12-04 16:25:02 -0300

    add contentido en components principal y nav-lateral

--- src/app/modules/administracion/admin-principal/components/principal/principal.component.ts
+++ src/app/modules/administracion/admin-principal/components/principal/principal.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/shared/services/auth.service';
 
 @Component({
   selector: 'app-principal',
@@ -7,4 +9,26 @@ import { Component } from '@angular/core';
 })
 export class PrincipalComponent {
 
+  constructor(
+    private router:Router,
+    private auth:AuthService
+  ){}
+
+  logout(){
+    this.auth.logout();
+    this.router.navigate(['/admin/login']);
+
+    setTimeout(() => {
+      window.location.reload();
+    }, 200);
+  }
+
+  getUser(){
+    return localStorage.getItem('usuario');
+  }
+
+  getRol(){
+    return localStorage.getItem('rol');
+  }
+  
 }
