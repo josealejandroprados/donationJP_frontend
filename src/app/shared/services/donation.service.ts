6dc2f56bb5aaf5df18018f21eda9906603292ee8
josealejandroprados <josealejandroprados@gmail.com>
2024-12-04 15:45:48 -0300

    add components principal y nav-lateral en module admin-principal mas enrutamiento de principal con hijos donations y users, add methods para get y delete de donations en donation.service

--- src/app/shared/services/donation.service.ts
+++ src/app/shared/services/donation.service.ts
@@ -1,9 +1,25 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DonationService {
 
-  constructor() { }
+  private baseUrl:string = environment.baseUrl;
+  
+  constructor(private http:HttpClient) { }
+
+  getDonations(){
+    return this.http.get<any>(`${this.baseUrl}/payments`);
+  }
+
+  getDonation(id:string){
+    return this.http.get<any>(`${this.baseUrl}/payment/${id}`);
+  }
+
+  deletePayment(id:string){
+    return this.http.delete<any>(`${this.baseUrl}/deletepayment/${id}`);
+  }
 }
